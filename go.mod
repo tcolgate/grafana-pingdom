78d056c414c72078a52eab2acb24d9fb679513d1
Tristan Colgate <tcolgate@gmail.com>
2018-12-07 15:59:00 +0000

    Start on prom exporter

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
 module github.com/tcolgate/grafana-pingdom
 
 require (
+	github.com/prometheus/client_golang v0.9.2
 	github.com/russellcardullo/go-pingdom v0.0.0-20181021024747-0897d314d9a6
 	github.com/tcolgate/grafana-simple-json-go v0.9.4
 )
 
-replace github.com/russellcardullo/go-pingdom => github.com/tcolgate/go-pingdom v0.0.0-20181205142829-a0c372a7de16
+replace github.com/russellcardullo/go-pingdom => github.com/tcolgate/go-pingdom v0.0.0-20181205204318-ff8ac1b65457
--- main.go
+++ main.go
@@ -5,26 +5,50 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"sort"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/russellcardullo/go-pingdom/pingdom"
 	simplejson "github.com/tcolgate/grafana-simple-json-go"
 )
 
-type Pingdom struct {
-	client *pingdom.Client
+type pingdomChecks interface {
+	List(...map[string]string) ([]pingdom.CheckResponse, error)
+	SummaryOutage(request pingdom.SummaryOutageRequest) (*pingdom.SummaryOutageResponse, error)
+	Results(request pingdom.ResultsRequest) (*pingdom.ResultsResponse, error)
 }
 
+// Pingdom implements a prom collector, and grafana simple json source for
+// scraping the pingdom API.
+type Pingdom struct {
+	client pingdomChecks
+}
+
+// GrafanaAnnotations implements the Annotator of the grafana-simple-json package.
 func (p *Pingdom) GrafanaAnnotations(ctx context.Context, query string, args simplejson.AnnotationsArguments) ([]simplejson.Annotation, error) {
-	chks, err := p.client.Checks.List()
+	chks, err := p.client.List()
+	if err != nil {
+		return nil, err
+	}
+
+	if query == "" {
+		query = ".*"
+	}
+
+	qre, err := regexp.Compile(query)
 	if err != nil {
 		return nil, err
 	}
 
 	var anns []simplejson.Annotation
 	for _, chk := range chks {
-		outs, err := p.client.Checks.SummaryOutage(pingdom.SummaryOutageRequest{
+		if !qre.MatchString(chk.Hostname) {
+			continue
+		}
+
+		outs, err := p.client.SummaryOutage(pingdom.SummaryOutageRequest{
 			From: int(args.From.Unix()),
 			To:   int(args.To.Unix()),
 			Id:   chk.ID,
@@ -34,7 +58,7 @@ func (p *Pingdom) GrafanaAnnotations(ctx context.Context, query string, args sim
 			continue
 		}
 
-		tags := []string{"down"}
+		tags := []string{"down", chk.Hostname}
 		for _, t := range chk.Tags {
 			tags = append(tags, t.Name)
 		}
@@ -48,7 +72,7 @@ func (p *Pingdom) GrafanaAnnotations(ctx context.Context, query string, args sim
 						Time:    time.Unix(out.From, 0),
 						TimeEnd: time.Unix(out.To, 0),
 						Title:   chk.Name,
-						Text:    chk.Name,
+						Text:    chk.Hostname,
 						Tags:    tags,
 					},
 				)
@@ -60,13 +84,54 @@ func (p *Pingdom) GrafanaAnnotations(ctx context.Context, query string, args sim
 	return anns, nil
 }
 
+var (
+	respThreshDesc = prometheus.NewDesc(
+		"pingdom_check_response_threshold_seconds",
+		"The alert threshold for thi check",
+		[]string{"name", "hostname"}, nil,
+	)
+	statusDesc = prometheus.NewDesc(
+		"pingdom_check_status_bool",
+		"",
+		[]string{"name", "hostname", "status"}, nil,
+	)
+	lastErrorTs = prometheus.NewDesc(
+		"pingdom_check_last_error_timestamp",
+		"Timestamp of the last error from a check",
+		[]string{"name", "hostname"}, nil,
+	)
+	lastTestTs = prometheus.NewDesc(
+		"pingdom_check_last_test_timestamp",
+		"Timestamp of the last test",
+		[]string{"name", "hostname"}, nil,
+	)
+	lastResponse = prometheus.NewDesc(
+		"pingdom_check_response_duration_seconds",
+		"Time taken for the last check.",
+		[]string{"name", "hostname"}, nil,
+	)
+)
+
+// Describe implements prometheus.Collector
+func (p *Pingdom) Describe(ch chan<- *prometheus.Desc) {
+	ch <- respThreshDesc
+	ch <- statusDesc
+	ch <- lastErrorTs
+	ch <- lastTestTs
+	ch <- lastResponse
+}
+
+// Collect implements prometheus.Collector
+func (p *Pingdom) Collect(ch chan<- prometheus.Metric) {
+}
+
 func main() {
 	client := pingdom.NewClient(
 		os.Getenv("EMAIL"),
 		os.Getenv("PASSWORD"),
 		os.Getenv("APIKEY"))
 
-	p := &Pingdom{client}
+	p := &Pingdom{client.Checks}
 	gsj := simplejson.New(
 		simplejson.WithAnnotator(p),
 	)
